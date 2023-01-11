e57a597a01993689bf348cc45687c917fac32c93
Ashwani Kumar <ashwani_kumar1@papajohns.com>
2023-01-11 23:28:24 +0530

    home endpoint added.

--- src/main/java/com/github/cicd/ComGithubCicdActionsApplication.java
+++ src/main/java/com/github/cicd/ComGithubCicdActionsApplication.java
@@ -2,9 +2,17 @@ package com.github.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ComGithubCicdActionsApplication {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome on docker actions.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComGithubCicdActionsApplication.class, args);
