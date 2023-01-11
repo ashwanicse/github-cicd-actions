b61a8304ff5eb7f82871771da50725e09f620809
Ashwani Kumar <ashwani_kumar1@papajohns.com>
2023-01-11 17:31:11 +0530

    Setup completed.

---
+++ src/main/java/com/github/cicd/ComGithubCicdActionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComGithubCicdActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComGithubCicdActionsApplication.class, args);
+	}
+
+}
