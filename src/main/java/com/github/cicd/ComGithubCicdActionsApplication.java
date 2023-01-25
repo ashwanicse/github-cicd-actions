ba0ef186705679ac3371c778071223900a455f2d
Ashwani Kumar <ashwani_kumar1@papajohns.com>
2023-01-25 11:51:41 +0530

    Added hostname

--- src/main/java/com/github/cicd/ComGithubCicdActionsApplication.java
+++ src/main/java/com/github/cicd/ComGithubCicdActionsApplication.java
@@ -1,5 +1,8 @@
 package com.github.cicd;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class ComGithubCicdActionsApplication {
-	
+
 	@GetMapping("/home")
 	public String home() {
-		return "Welcome on docker actions.";
+		String hostName = "Default";
+		try {
+			hostName = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return "Welcome on docker actions. - hostName: " + hostName;
 	}
 
 	public static void main(String[] args) {
